53751371613ae16bccaa762f2e1cccb24cf886a7
GuilhermeOSR <guilherme.o.santarosa@gmail.com>
2019-11-06 18:42:08 -0300

    Projeto 013 Finalizado

--- pm-proj013/src/programa1.js
+++ pm-proj013/src/programa1.js
@@ -7,7 +7,13 @@
 *******************************************************************************/
 
 
-
+            function termostato(temp) {
+                if(temp < 0) {
+                    return "Temperatura negativa";
+                } else{
+                    return "Temperatura positiva";
+                }
+            }
 
 
 
@@ -22,9 +28,19 @@
 *******************************************************************************/
 
 
-
-
-
+             function boletimEscolar(nota) {
+                 if(nota > 10 || nota < 0 ) {
+                    return "Nota inválida"; 
+                 } else if(nota < 6) {
+                     return "Reprovado";
+                 } else if(nota >= 6) {
+                     return "Aprovado";
+                 } else {
+                     return "Error";
+                 }
+             }
+                                        
+                                       
 
 
 /*******************************************************************************
@@ -38,11 +54,18 @@
 		- Outros -> 35,90
 *******************************************************************************/
 
-
-
-
-
-
+        function calcularFrete(estado) {
+            if(estado == "MG") {
+                return 12.50;
+            } else if(estado == "SP") {
+                return 23.90;
+            } else if(estado == "RJ") {
+                   return 18.20;
+                } else {
+                    return 35.90;
+                }
+        }
+ 
 
 /*******************************************************************************
 	Tarefa 4:
@@ -53,10 +76,18 @@
 		- mais de um 1,85 -> "joga basquete?"
 *******************************************************************************/
 
-
-
-
-
+          function calcularAltura(altura) {
+              if(altura < 1.60) {
+                  return "baixinho";
+              } else if(altura >= 1.60 && altura <= 1.85 ) {
+                  return "altura normal";
+              } else if(altura > 1.85) {
+                  return "joga basquete?";
+              } else {
+                  return "Error";
+              }
+          }
+          
 
 
 /*******************************************************************************
@@ -65,3 +96,20 @@
 		retorna a mensagem "Login efetuado com sucesso", senão, retorna a mensagem
 		"Senha inválida".
 *******************************************************************************/
+
+
+            function validarSenha(senha) {
+                
+                if(senha == '123456') {
+                   return "Login efetuado com sucesso";
+                } else if(senha != '123456') {
+                    return "Senha inválida";
+                } else {
+                    return "error";
+                }
+            }
+
+
+
+
+
